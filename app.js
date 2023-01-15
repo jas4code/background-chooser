fe5c5487443678010d3244555f1de21a01e610d2
jas4code <jasabocejo@protonmail.com>
2023-01-15 15:54:03 +0800

    Made the code more cleaner

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 
 $(document).ready(function(){
 
-
     $('#main_box').click(function(){
         $('#main_box').css("background-image", "linear-gradient(to bottom right, #94a3b8, #475569)");
         $('img').css("border", "none");
         //comment this out when you have figured out what event.stopPropagation is used for
      })
+     
      $('.mini_boxes').click(function(event){
         event.stopPropagation(); // an improtant function to remember
         $('#main_box').css("background-image", "url(" + $(this).attr("src")) + ")";
