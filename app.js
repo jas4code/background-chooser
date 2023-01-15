42417e66bff68d31384323f19291987d3c17fa75
jas4code <jasabocejo@protonmail.com>
2023-01-15 15:48:09 +0800

    Refactored my logic file
    added the functionalities needed and added a demo video

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-$(document).ready(function(){
-    // $('#main_box').click(function(){
-    //     alert('you clicked the main box!');  
-    //     //comment this out when you have figured out what event.stopPropagation is used for
-    //  })
-    //  $('.mini_boxes').click(function(event){
-    //     event.stopPropagation(); // improtant function to remember
-    //     alert("you clicked a mini box!");
-    //  })
 
-     $('.mini_boxes').hover(function() {
+$(document).ready(function(){
+
+
+    $('#main_box').click(function(){
+        $('#main_box').css("background-image", "linear-gradient(to bottom right, #94a3b8, #475569)");
+        $('img').css("border", "none");
+        //comment this out when you have figured out what event.stopPropagation is used for
+     })
+     $('.mini_boxes').click(function(event){
+        event.stopPropagation(); // an improtant function to remember
         $('#main_box').css("background-image", "url(" + $(this).attr("src")) + ")";
-    })
+        $('img').css("border", "none");
+        $(this).css("border", "2px solid #facc15");
+     })
 });
